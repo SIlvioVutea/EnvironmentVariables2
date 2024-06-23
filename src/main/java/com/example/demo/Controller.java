3f5f474e2ef1541ef9276836159850fab84852a8
Worro <alessiodelledonne29@gmail.com>
2024-06-23 14:45:11 +0200

    created exercise

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String greet(){
+        return environment.getProperty("myCustomVarTree.welcomeMsg");
+    }
+}
